ea192874af38b209238a61f137d6ff56612b9d59
mannnu-ai <gamingvideos044@gmail.com>
2025-06-02 20:33:54 +0530

    Delete payment.py

--- payment.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-def is_premium_user(user_id):
-    conn = sqlite3.connect("database.db")
-    c = conn.cursor()
-    c.execute("SELECT premium FROM users WHERE id=?", (user_id,))
-    row = c.fetchone()
-    conn.close()
-    return row and row[0] == 1
